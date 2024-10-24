bbd490a25f16557df7cd2d3161c4e718734f4e95
noname <danpod1001@mail.ru>
2024-10-24 21:36:05 +0300

    cosmetic changes

--- src/back/Translator.cpp
+++ src/back/Translator.cpp
@@ -9,9 +9,9 @@
 void Translator::run() {
     Lexeme lexeme = lex_analyzer_.getLexeme();
     while (lexeme.getType() != LexemeType::EndOfFile) {
-        std::cout << "Lexeme: " << lexeme.getName() << "    ";
-        std::cout << "Type: " << lexeme.toString() << "    ";
-        std::cout << "Line: " << lexeme.getId() << '\n';
+        std::cout << "\e[1;32mLexeme: \e[1;33m" << lexeme.getName() << "\e[0m    ";
+        std::cout << "\e[1;32mType: \e[0m" << lexeme.toString() << "    ";
+        std::cout << "\e[1;32mLine: \e[0m" << lexeme.getId() << '\n';
         std::cout << "-------------------------------------\n";
         lexeme = lex_analyzer_.getLexeme();
     }
